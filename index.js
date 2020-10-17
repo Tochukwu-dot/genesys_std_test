b4deee44c8fa68e41c0f1eb5b81b09f2b10b4788
Tochukwu-dot <nwibetochukwu@gmail.com>
2020-10-17 23:04:45 +0100

    first modification

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Joi = require('joi');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const customers = require('./DB/customers');
+const customers = require('./DB/menu');
+const menu = require('./DB/menu');
 
 mongoose.connect('mongodb://localhost/menu')
     .then(() => console.log('CONNECTED TO MONGODB'))
@@ -10,36 +11,68 @@ mongoose.connect('mongodb://localhost/menu')
 
 app.use(express.json());
 
-// to display list of customers
-app.get('/api/customers', (req, res) => {
-    res.send(customers);
+// to display the menu
+app.get('/api/menu', (req, res) => {
+    res.send(menu);
 });
 
-// to create a customer
-app.post('/api/customers', (req, res) => {
-    const { error } = validateCustomers(req.body)
-    if (error) return res.status(400).send(result.error.details[0].message);
+// to display a particular menu item
+app.get('/api/menu/:id', (req, res) => {
+    const menuItem = menu.find(c => c.id === parseInt(req.params.id));
+    if (!menuItem) return res.status(404).send('The menuItem with the given ID was not found.');
+    res.send(menuItem);
+  });
+  
 
-    //Get the customer data
-    let customer = {
-        id: customers.length + 1,
+// to create a food item
+app.post('/api/menuItem', (req, res) => {
+    const { error } = validateMenu(req.body)
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //Get the food item data
+    let menuItem = {
+        id: menu.length + 1,
         name: req.body.name,
-        age: req.body.age
+        price: req.body.price
     };
-    //insert the new customer to the customers array
-    customers.push(customer)
+    //insert the new  to the customers array
+    menu.push(menuItem)
     //return the newly added customer
-    res.send(customer);
+    res.send(menuItem);
 });
 
+//updating the menu
+app.put('/api/menu/:id', (req, res) => {
+    const menuItem = menu.find(c => c.id === parseInt(req.params.id));
+    if (!menuItem) return res.status(404).send('The item with the given ID was not found.');
+  
+    const { error } = validateMenu(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    menuItem.name = req.body.name; 
+    res.send(menuItem);
+  });
 
+// deleting an item
+app.delete('/api/menu/:id', (req, res) => {
+    const menuItem = menu.find(c => c.id === parseInt(req.params.id));
+    if (!menuItem) return res.status(404).send('The item with the given ID was not found.');
+  
+    const index = menu.indexOf(menuItem);
+    menu.splice(index, 1);
+  
+    res.send(`item deleted successfully`);
+  });
+  
 
-function validateCustomers(customer) {
+  // validating input for menu items
+function validateMenu(menuItem) {
     const schema = {
-      name: Joi.string().min(3).required()
+        name: Joi.string().min(3).required(),
+        price: Joi.number().min(2).required()
     };
   
-    return Joi.validate(customer, schema);
+    return Joi.validate(menuItem, schema);
   }
   
 
